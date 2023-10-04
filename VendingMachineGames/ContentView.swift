6e7f60c7a90a8246bef8bb73f4e434d1c5ca5a81
dekapd99 <dekaprimatio@gmail.com>
2023-10-04 09:12:26 +0700

    Initial Commit

---
+++ VendingMachineGames/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VendingMachineGames
+//
+//  Created by Deka Primatio on 04/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VendingMachineGames/VendingMachineGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendingMachineGamesApp.swift
+//  VendingMachineGames
+//
+//  Created by Deka Primatio on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct VendingMachineGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
